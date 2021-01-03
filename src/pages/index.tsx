b5e7f51f1c7d4fa2c5d1281139d35e91b804cc42
Marco Acierno <marcoaciernoemail@gmail.com>
2021-01-03 17:59:02 +0000

    Add sponsors draft

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
+import { Box } from "theme-ui";
 import { About } from "~/sections/about";
 import { Hero } from "~/sections/hero";
 import { Schedule } from "~/sections/schedule";
+import { Sponsors } from "~/sections/sponsors";
 
-const Home = () => {
-  return (
-    <div>
-      <Hero />
-      <About />
-      <Schedule />
-    </div>
-  );
-};
+const Home = () => (
+  <Box>
+    <Hero />
+    <About />
+    <Schedule />
+    <Sponsors />
+  </Box>
+);
 
 export default Home;
