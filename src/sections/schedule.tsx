77676ce975cbd61087c51fe83285a671a2809f09
Marco Acierno <marcoaciernoemail@gmail.com>
2021-01-03 19:21:28 +0000

    Get schedule data from the dataset

--- src/sections/schedule.tsx
+++ src/sections/schedule.tsx
@@ -1,17 +1,19 @@
 import { Container, Heading, Paragraph } from "theme-ui";
 import { ScheduleCard } from "~/components/schedule-card";
+import { SCHEDULE } from "~/data/schedule";
 
 export const Schedule = () => (
   <Container variant="smallContainer">
     <Heading>Schedule</Heading>
-    <Paragraph>The schedule is displayed in</Paragraph>
+    <Paragraph>The schedule is displayed in your timezone</Paragraph>
 
-    <ScheduleCard variant="info" />
-    <ScheduleCard />
-    <ScheduleCard invert />
-    <ScheduleCard />
-    <ScheduleCard invert />
-    <ScheduleCard />
-    <ScheduleCard invert />
+    {SCHEDULE.map((schedule, index) => (
+      <ScheduleCard
+        variant={schedule.label ? "info" : "talk"}
+        key={schedule.name}
+        schedule={schedule}
+        invert={!Boolean(index % 2)}
+      />
+    ))}
   </Container>
 );
